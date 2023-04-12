fd415883ec723343d5e7090e2b86219ce6f0a674
lucas-alexandrino <lucas.alexcontato@gmail.com>
2023-04-12 17:50:35 -0300

    Add FlexBox

---
+++ src/components/statics/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Footer () {
+    <>
+    
+    </>
+}
+
+export default Footer;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./home.css";
-import { Box } from "@mui/material";
+import { Box , Button } from "@mui/material";
 import { Paper } from "@material-ui/core";
 
 function Home() {
   return (
     <>
       <Paper>
-        <Box>
-          <Box>
+        <Box p={2}>
+          <Box display="flex" justifyContent="center">
             <h1>Titulo</h1>
+          </Box>
+          <img src="lucas-alexandrino.github.io" alt="algo" style={{width: "100%", height: "100%"}} />
+          <Box display="flex" justifyContent="center" p={2}>
+            <Button variant="contained" color="primary">Texto1</Button>
+            <Button variant="contained" color="secondary">Texto2</Button>
           </Box>
         </Box>
       </Paper>

036279d6c58f2d09852ba4914c2558acf3bde2dc
lucas-alexandrino <lucas.alexcontato@gmail.com>
2023-04-12 15:39:41 -0300

    Input Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './paginas/home/Home'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./paginas/home/Home";
+import { Grid } from "@material-ui/core";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <Home />
-  )
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import "./home.css";
-import { Grid, Paper } from "@material-ui/core";
+import { Box } from "@mui/material";
+import { Paper } from "@material-ui/core";
 
 function Home() {
   return (
     <>
-      <Grid container>
-        <Grid item xs={12} sm={4}>
-          <Paper style={{ height: "100vh", background: "lightgrey" }} />
-        </Grid>
-        <Grid item container direction="row-reverse" spacing={2}>
-          <Grid item sm={4}>
-            <Paper style={{ height: "49vh", background: "orange" }} />
-          </Grid>
-          <Grid item sm={4}>
-            <Paper style={{ height: "49vh", background: "grey" }} />
-          </Grid>
-        </Grid>
-      </Grid>
+      <Paper>
+        <Box>
+          <Box>
+            <h1>Titulo</h1>
+          </Box>
+        </Box>
+      </Paper>
     </>
   );
 }

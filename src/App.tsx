ee299ca9ba12b0032bc8ffe73065307aa1256897
lucas-alexandrino <lucas.alexcontato@gmail.com>
2023-04-20 16:51:23 -0300

    Adicionando Componentes Post e Temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,56 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
-import Navbar from "./components/statics/navbar/Navbar";
-import Footer from "./components/statics/footer/Footer";
-
-import "./App.css";
-import Home from "./paginas/home/Home";
-import Login from "./paginas/login/Login";
-import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
-import ListaTema from "./components/temas/listatema/ListaTema";
-import ListaPostagem from "./components/postagens/listapostagem/ListaPostagem";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <Router>
       <Navbar />
-      <BrowserRouter>
+      <div style={{ minHeight: '100vh' }}>
         <Routes>
-        <Route path="/" element={<Login />} />;
-        <Route path="/login" element={<Login />} />;
-          <Route path="/home" element={<Home />} />;
-          <Route path="/cadastro" element={<CadastroUsuario/>}/>
-          <Route path="/posts" element={<ListaPostagem/>}/>
-          <Route path="/temas" element={<ListaTema/>}/>
+
+          <Route path="/" element={<Login />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/home" element={<Home />} />
+
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/posts" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+
         </Routes>
-      </BrowserRouter>
+      </div>
       <Footer />
-    </>
+
+    </Router>
   );
 }
 
--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -1,40 +1,72 @@
-import { MenuItem } from "@material-ui/core";
-import { AccountCircle, Search } from "@material-ui/icons";
-import {
-  AppBar,
-  Badge,
-  Box,
-  IconButton,
-  Menu,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import MenuIcon from "@mui/icons-material/Menu";
+import React from 'react';
+import { AppBar, Toolbar, Typography} from '@material-ui/core';
+import {Box} from '@mui/material';
+import { Link } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom'
+import useLocalStorage from 'react-use-localstorage';
 
+import './Navbar.css'
 function Navbar() {
-  return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton 
-              
-              className="button_icon">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Postagens
-            </Typography>
-            <Button id='buttonLogin'>Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      
-    </>
-  );
+    const [token, setToken] = useLocalStorage('token');
+    let navigate = useNavigate();
+    
+    function goLogout(){
+        setToken('')
+        alert("Usuário deslogado")
+       navigate('/login')
+    }
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start" >
+                        <Link to="/home" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    home
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/temas" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to="/formularioTema" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                        </Link>
+                      
+                            <Box mx={1} className='cursor' onClick={goLogout}>
+                                <Typography variant="h6" color="inherit">
+                                    logout
+                                </Typography>
+                            </Box>
+                        
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    )
 }
 
 export default Navbar;
